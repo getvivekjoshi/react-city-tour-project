8e1cb9f14b79ffd8bcfe5de7f4f663a32c05637d
getvivekjoshi <getvivekjoshi@hotmail.com>
2019-03-25 16:39:04 +0530

    first commit

---
+++ citytour/precache-manifest.251c496bea9367f336e08e97220c94a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/static/media/logo.12a6659f.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fae7c8d48bcdb68fb828",
+    "url": "/static/js/main.fae7c8d4.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "fae7c8d48bcdb68fb828",
+    "url": "/static/css/main.ca7c3b55.chunk.css"
+  },
+  {
+    "revision": "11c1c7b1b5958ed304d123c03e28a50b",
+    "url": "/index.html"
+  }
+];
